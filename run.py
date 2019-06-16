e77005c9a283b5e3293df4b20cf1b910e4ce3f13
thepsalmist <xf.xavierfrank@gmail.com>
2019-06-16 12:24:23 +0300

    create a funtion that creates new user on sign-up

--- run.py
+++ run.py
@@ -1,7 +1,24 @@
-import User from user
-import Credentials from credentials
+from user import User
+from credentials import Credentials
+
+
+def create_account(first_name, last_name, password):
+    """
+    Fuction that creates a new user account when a user opts to sign-up
+    """
+    new_user = User(first_name, last_name, password)
+    return new_user
 
 
 def main():
     print(" ")
-    print("Welcome to Your Password Vault")
+    print("WELCOME TO YOUR PASSWORD VAULT!!!")
+    print("-"*100)
+    while True:
+        print(" ")
+        print("Use the shortcodes below to navigate:\n 1. SU- Sign Up\n 2. LI- Log in to your\n 3. EX- Exit ")
+        short_code = input("Short code:").lower()
+        if short_code == "ex":
+            break
+
+        elif short_code == "su":
--- user.py
+++ user.py
@@ -4,12 +4,13 @@ class User:
     """
     users = []
 
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, password):
         """
         Method to create instances of class User
         """
         self.first = first_name
         self.last = last_name
+        self.password = password
         self.username = '@' + first_name
         self.email = first_name + '.' + last_name + '@gmail.com'
 
