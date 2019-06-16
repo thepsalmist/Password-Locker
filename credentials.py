192fa1b9bf8d6b7b0b623d892345ea4be204889d
thepsalmist <xf.xavierfrank@gmail.com>
2019-06-16 11:11:35 +0300

    creat main function

--- credentials.py
+++ credentials.py
@@ -1,5 +1,6 @@
 class Credentials:
     """
-    Class to store user credentials
+    Class to create and store user credentials
     """
-    pass
+
+    def __init__(self, first_name)
---
+++ run.py
@@ -0,0 +1,7 @@
+import User from user
+import Credentials from credentials
+
+
+def main():
+    print(" ")
+    print("Welcome to Your Password Vault")
