c80ba645ce38f2e50d7c0b6977781df14bafe914
thepsalmist <xf.xavierfrank@gmail.com>
2019-06-16 23:43:27 +0300

    add test method to test that new users are saved to users list

--- user.py
+++ user.py
@@ -8,8 +8,8 @@ class User:
         """
         Method to create instances of class User
         """
-        self.first = first_name
-        self.last = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.password = password
         self.user_name = user_name
         self.email = first_name + '.' + last_name + '@gmail.com'
--- user_test.py
+++ user_test.py
@@ -1,5 +1,5 @@
 import unittest
-import user
+from user import User
 
 
 class TestUser(unittest.TestCase):
@@ -24,6 +24,13 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.last_name, "Gianis")
         self.assertEqual(self.new_user.user_name, "Dinos254")
         self.assertEqual(self.new_user.password, 'pass12345')
+
+    def test_save_user(self):
+        """
+        Test to asertain that the new user is saved into the users list and check the users list length
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.users), 1)
 
 
 if __name__ == '__main__':
