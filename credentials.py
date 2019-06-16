2a75c2c37de9aa8907b094b547c6d63335092a1d
thepsalmist <xf.xavierfrank@gmail.com>
2019-06-16 13:29:42 +0300

    create method that initializes credential instamces and saves new user credentials

--- credentials.py
+++ credentials.py
@@ -7,5 +7,19 @@ class Credentials:
     """
     Class to create and store user credentials
     """
+    user_credential = []
 
-    def __init__(self, first_name)
+    def __init__(self, website, username, account_name, password):
+        """
+        Method to create instances of class Credentials
+        """
+        self.website = website
+        self.username = username
+        self.account_name = account_name
+        self.password = password
+
+    def save_credentials(self):
+        """
+        Function that saves new credentials instances
+        """
+        Credentials.user_credential.append(self)
