73a0406f6a7ef6af49688c767e14571a28dc6b88
thepsalmist <xf.xavierfrank@gmail.com>
2019-06-16 12:52:17 +0300

    create a method to verify user before login

--- run.py
+++ run.py
@@ -10,6 +10,13 @@ def create_account(first_name, last_name, password):
     return new_user
 
 
+def save_account(user):
+    """
+    Function to save a new user account on sign-up
+    """
+    User.save_user(user)
+
+
 def main():
     print(" ")
     print("WELCOME TO YOUR PASSWORD VAULT!!!")
@@ -22,3 +29,17 @@ def main():
             break
 
         elif short_code == "su":
+            print(" ")
+            print("-"*100)
+            print("Fill in the details below to Sign-up for a new account:")
+            first_name = input("First Name:")
+            last_name = input("Last Name:")
+            username = '@' + first_name
+            password = input("Enter Your Password")
+            save_account(create_account(first_name, last_name, password))
+            print("-"*10)
+            print(f'Account succesfully created, username {username}, password {password}.')
+        elif short_code == "li":
+            print("")
+            print("-"*100)
+            print("Log In with your username and password:")
