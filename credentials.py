82ad5f0025059853466fd11483f990e6c10cc7d7
thepsalmist <xf.xavierfrank@gmail.com>
2019-06-16 17:23:14 +0300

    create a function to authenticate users on login

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 from user import User
+from random import choice
 import string
-import random
 
 
 class Credentials:
@@ -9,12 +9,12 @@ class Credentials:
     """
     user_credential = []
 
-    def __init__(self, website, username, account_name, password):
+    def __init__(self, website, user_name, account_name, password):
         """
         Method to create instances of class Credentials
         """
         self.website = website
-        self.username = username
+        self.user_name = user_name
         self.account_name = account_name
         self.password = password
 
@@ -23,3 +23,24 @@ class Credentials:
         Function that saves new credentials instances
         """
         Credentials.user_credential.append(self)
+
+    def random_password():
+        """
+        Function that generates an eight-character alphanumeric password
+        """
+        alphabet = string.ascii_letters + string.digits
+        password = ''.join(choice(alphabet) for i in range(8))
+
+    @classmethod
+    def auth_user(cls, user_name, password):
+        """
+        Function to authenticate user by checking the name and password against the users array
+        """
+        present_user = ""
+        for user in User.users:
+            if(user.user_name == user_name and user.password == password):
+                present_user = user.user_name
+            return present_user
+
+    @classmethod
+    def display_credentials(cls,)
--- user.py
+++ user.py
@@ -4,13 +4,14 @@ class User:
     """
     users = []
 
-    def __init__(self, first_name, last_name, password):
+    def __init__(self, first_name, last_name, user_name password):
         """
         Method to create instances of class User
         """
         self.first = first_name
         self.last = last_name
         self.password = password
+        self.user_name = user_name
         self.email = first_name + '.' + last_name + '@gmail.com'
 
     def save_user(self):
